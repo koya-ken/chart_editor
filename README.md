746b94c79cc970065b045c0eddae86a8d83fc997
koya-ken <koya.smp@gmail.com>
2023-10-15 10:37:42 +0900

    update css

--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 ## reference
 ## css animation
 https://white-azalea.hatenablog.jp/entry/2015/05/24/000853
+
+## css
+http://creator.aainc.co.jp/archives/4947
+
+## javascript event
+
+https://zero-plus.io/media/mouseenter-mouseleve/
+
+## react grid layout
+
+https://github.com/react-grid-layout/react-grid-layout
--- js/main.js
+++ js/main.js
@@ -13,22 +13,23 @@ class kanban {
             element.classList.add('click-item');
             var width = event.currentTarget.clientWidth;
             var height = event.currentTarget.clientHeight;
-            element.base_style = `width: ${width}px; height:${height}px; background-color: aqua;`;
+            element.base_style = `width: ${width}px; height:${height}px;`;
             element.style = `${element.base_style} left:${event.x - width / 2}px;top: ${event.y - height / 2}px;`;
             var clickElement = event.currentTarget;
             window.addEventListener('mousemove', function (event2) {
                 event2.preventDefault();
                 element.style = `${element.base_style} left:${event2.x - width / 2}px;top: ${event2.y - height / 2}px;`;
             })
+            /**
+             * https://stackoverflow.com/questions/4950115/removeeventlistener-on-anonymous-functions-in-javascript
+             */
             window.addEventListener('mouseup', function _listener(event2) {
                 event2.preventDefault();
-                console.log('mouseup');
                 element.remove();
                 clickElement.classList.remove('ismoving');
                 window.removeEventListener('mouseup', _listener);
             })
 
-            console.log(event.currentTarget.offsetWidth);
             event.currentTarget.classList.add('ismoving');
             document.body.appendChild(element);
         });
